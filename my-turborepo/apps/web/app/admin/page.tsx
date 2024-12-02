305714dc1067463a6ddb3ff2872e281f20dfef4f
Sabbir Tanvir <sabbirmahmudtanvir@gmail.com>
2024-12-02 22:55:20 +0600

    k

---
+++ my-turborepo/apps/web/app/admin/page.tsx
@@ -0,0 +1,8 @@
+import { Admin } from "@repo/ui/admin";
+
+export default function () {
+    return <div>
+        <h1>Admin</h1>
+        <Admin />
+    </div>
+}
---
+++ my-turborepo/packages/ui/src/admin.tsx
@@ -0,0 +1,7 @@
+export function Admin() {
+    return <div>
+        <h1>Admin</h1>
+        <p>Admin page</p>
+    </div>
+}
+
